dd5e53b3dd64d882d58e4c60549ad210b9049b2b
reynolda <vladisalv.drv@yandex.ru>
2026-01-13 21:35:05 +0300

    Added: uniq naming of the logs file

--- log/logging.go
+++ log/logging.go
@@ -4,10 +4,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func Logging(returnedErr error) {
-	file, err := os.OpenFile("logs.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	filename := "logs.log"
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("logging: cannot get filepath: %s\n", err.Error())
+	} else {
+		path := strings.Split(pwd, "/")
+		fmt.Println(path)
+		temp := filename
+		filename = path[0]
+		for i, val := range path {
+			if i == 0 {
+				continue
+			}
+			filename += val + "_"
+		}
+		filename += temp
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		if os.IsPermission(err) {
 			fmt.Printf("logging: cannot create or open logfile, premission denied: %s\n", err.Error())
